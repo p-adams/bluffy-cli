2e56677e21d6f461e30faa594e6cfefd89e8c592
Paul Adams <paadams@nmu.edu>
2018-07-22 14:15:53 -0500

    create simple CLI

--- src/bluffy_cli.nim
+++ src/bluffy_cli.nim
@@ -1,32 +1,58 @@
-
+import os
+import json
 # welcome message
 echo ""
 echo "Welcome to Bluffy"
 echo "A CLI for generating fake JSON data"
 echo ""
 
-echo "Define a JSON schema for your data"
-var jsonScheme = readLine(stdin)
-while jsonScheme == "":
-    echo "You must define a JSON schema"
-    jsonScheme = readLine(stdin)
+# writeFile("meow.json", $(%* {"foo": "bar"}))
+var
+    jsonSchema: string = ""
+    jsonFields: string = ""
+    fieldsMatchSchema: bool = false
 
 
-echo "Define JSON fields based on schema"
-var jsonFields = readLine(stdin)
-while jsonFields == "":
-    echo "You must define JSON fields"
+
+while not fieldsMatchSchema:
+    echo "On a single line, define a JSON schema for your data (default is {}):"
+    jsonSchema = readLine(stdin)
+    while jsonSchema == "":
+        # default to empty JSON object
+        jsonSchema = $(%*"{}")
+
+
+    echo "On a single line, define JSON fields based on schema (default is {}):"
     jsonFields = readLine(stdin)
+    while jsonFields == "":
+        # default to empty JSON object
+        jsonFields = $(%*"{}")
+    #[
+        This is where JSON schema validation will take place
+        Refer: https://github.com/PMunch/jsonschema
+        if jsonSchema == jsonFields:
+            echo "JSON fields match the schema"
+            fieldsMatchSchema = true
+        else:
+            echo "JSON fields did not match the schema"
+    ]#
+    fieldsMatchSchema = true
 
-echo "Choose a file name"
+echo "Choose a file name:"
 var filename = readLine(stdin)
 while filename == "":
     echo "You must choose a file name"
     filename = readLine(stdin)
+
+filename.add(".json")
+
+# write JSON to file
+writeFile(filename, $(pretty(%* parseJSON(jsonFields), 2)))
 
 
 #[
     Todos:
         create styledEcho for errors, info/notifaction/ success message
         (stdout.write for all other interactions)
+        add json schema validator
 ]#
